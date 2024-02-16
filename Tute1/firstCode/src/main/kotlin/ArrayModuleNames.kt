bcb002f5a6f2af469e352d5cdca10775420fb5a6
VikumChathuranga22434 <127230237+VikumChathuranga22434@users.noreply.github.com>
2024-02-16 23:30:14 +0530

    tute 1 MAD

---
+++ Tute1/firstCode/src/main/kotlin/ArrayModuleNames.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var arrayModule = arrayOf("MAD", "DSA", "ITP", "PS", "PROS", "ESD")
+
+    for (i in arrayModule)
+        println(i)
+}
---
+++ Tute1/firstCode/src/main/kotlin/Variables.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val intVal = 1 // integer variable
+    val longVal = 1L // long integer
+    val byteVal: Byte = 1 // if we initializing the byte we have to set it into byte variable type
+
+    // printing output
+    println("Integer Value = $intVal")
+    println("Long Value = $longVal")
+    println("Byte Value = $byteVal")
+
+}
